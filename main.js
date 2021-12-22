984e45299a1d1dea53715c7dc38d5d8707792fc6
jafcoder <joefarr113@gmail.com>
2021-12-22 15:54:29 +0000

    idea? donw

--- main.js
+++ main.js
@@ -2,4 +2,22 @@
 👉 Write your kata here!
 */
 
+/* "Oh no! Santa's elves have made a mess of the Naughty List and the names have all been mixed with the Nice List. 
+Fortunately, the elves separate the naughty from the nice with the use of lowercase letters.
+You will be given a string containing a mix of both upper and lowercase letters.
+You will need to write a function to separate the lowercase letters to find out the name of the person on the Naughty List.
+
+ - The name will be spread out among the letters but will remain in the right order.
+
+
+KDFjJLAoSKJsDFNLJKCNMPEWeOIFPQWpOIKCMLASKDChNALSDJKFN == joseph
+
+*/
+
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function returnTheNaughtyListee(){
+
+
+}
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,29 @@
 //👉 Write your tests below here:
+
+KDFJLASKJDFNLJKCNMPEWOIFPQWOIKCMLASKDCNALSDJKFN
+
+import { returnTheNaughtyListee } from "./main.js"
+
+test('The function should return from "KDFjJLAoSKJsDFNLJKCNMPEWeOIFPQWpOIKCMLASKDChNALSDJKFN" the name "joseph', function(){
+    //arrange
+    const jumbled = "KDFjJLAoSKJsDFNLJKCNMPEWeOIFPQWpOIKCMLASKDChNALSDJKFN"
+    const expected = "joseph"
+    //act
+    const actual = returnTheNaughtyListee(jumbled);
+
+    //assert
+    expect(actual).toBe(expected);
+})
+
+"KDFjJLAoSKJsDFNLJKCNMPEWeOIFPQWpOIKCMLASKDChNALSDJKFN"
+
+test('The function should return from "KDFjJLAoSKJsDFNLJKCNMPEWeOIFPQWpOIKCMLASKDChNALSDJKFN" the name "joseph', function(){
+    //arrange
+    const jumbled = "KDFJLASKJDFNLJKCNMPEWOIFPQWOIKCMLASKDCNALSDJKFN"
+    const expected = "joseph"
+    //act
+    const actual = returnTheNaughtyListee(jumbled);
+
+    //assert
+    expect(actual).toBe(expected);
+})
