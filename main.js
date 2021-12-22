be58452bde4583e4d3838c0ec3b6d002bd849203
jafcoder <joefarr113@gmail.com>
2021-12-22 16:25:31 +0000

    update

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 👉 Write your kata here!
 */
 
-/* "Oh no! Santa's elves have made a mess of the Naughty List and the names have all been mixed with the Nice List. 
-Fortunately, the elves separate the naughty from the nice with the use of lowercase letters.
+/* "Oh no! Santa's elves have made a mess of the Nice List and the names have all been mixed with the Naughty List. 
+Fortunately, the elves separate the naughty kids using UPPERCASE letters.
 You will be given a string containing a mix of both upper and lowercase letters.
-You will need to write a function to separate the lowercase letters to find out the name of the person on the Naughty List.
+You will need to write a function to separate the lowercase letters to find out the name of the person on the Nice List.
 
  - The name will be spread out among the letters but will remain in the right order.
 
